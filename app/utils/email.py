178fac1ac8aa6dab84f8ac814b9906abab7aba08
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2026-02-08 08:32:00 +0100

    update email sending to include HTML templates

--- app/utils/email.py
+++ app/utils/email.py
@@ -1,22 +1,28 @@
-from flask import current_app, url_for
+from flask import current_app, url_for, render_template
 from flask_mail import Message
 
 
 def send_verification_email(user, token):
     verify_url = url_for("auth.verify_email", token=token, _external=True)
+    html_body = render_template("email/verify.html", user=user, verify_url=verify_url)
+    text_body = f"Hello {user.first_name},\n\nPlease verify your email by clicking the link below:\n\n{verify_url}\n\nThis link expires in 24 hours.\n\nIf you did not create an account, ignore this email."
     msg = Message(
         subject="Verify your Matcha account",
         recipients=[user.email],
-        body=f"Hello {user.first_name},\n\nPlease verify your email by clicking the link below:\n\n{verify_url}\n\nThis link expires in 24 hours.\n\nIf you did not create an account, ignore this email.",
+        body=text_body,
+        html=html_body,
     )
     current_app.extensions["mail"].send(msg)
 
 
 def send_password_reset_email(user, token):
     reset_url = url_for("auth.reset_password_confirm", token=token, _external=True)
+    html_body = render_template("email/reset_password.html", user=user, reset_url=reset_url)
+    text_body = f"Hello {user.first_name},\n\nTo reset your password, click the link below:\n\n{reset_url}\n\nThis link expires in 1 hour.\n\nIf you did not request a password reset, ignore this email."
     msg = Message(
         subject="Reset your Matcha password",
         recipients=[user.email],
-        body=f"Hello {user.first_name},\n\nTo reset your password, click the link below:\n\n{reset_url}\n\nThis link expires in 1 hour.\n\nIf you did not request a password reset, ignore this email.",
+        body=text_body,
+        html=html_body,
     )
     current_app.extensions["mail"].send(msg)
